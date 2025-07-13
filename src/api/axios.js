e117eb4e2bf3274173c903c5580921edfd065371
Omegatroy <juli.mjoi44@gmail.com>
2025-07-13 17:25:26 -0300

    fix: corregir import api_url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-//import { API_URL } from "../config";
+import { API_URL } from "../config";
 
-const API_URL = "http://localhost:4000/api";
 const instance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
